87f4ecb5ac5b4c9b71e4f384b1496cef3c172459
Son Nguyen Tran Hong <son.nguyen2@orientsoftware.com>
2026-01-28 09:25:52 +0700

    day1 - exercise: greeting by username (ask for input)

---
+++ day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the C# programming world!");
+
+/* 
+    Console.ReadLine();
+        Using for reading input from the user.
+        Only take string input.
+*/
+
+// Exercise: Take user name as input and greet the user with his name.
+Console.Write("Enter your name: ");
+string? username = Console.ReadLine();
+
+System.Console.WriteLine($"Hello, {username}. Welcome!");
+
+
+// ==========================================================
