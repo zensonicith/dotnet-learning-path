956d38e1f0ddb8ab15d01269b6f780475882415a
Son Nguyen Tran Hong <son.nguyen2@orientsoftware.com>
2026-01-30 16:17:43 +0700

    coninue research about arrays, methods

---
+++ day4/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+
+// ====================================================
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
